36de4f5fd5f19de3428328fb8ea9d2a6521e20e5
Drako Hyena <79679437+DrakoHyena@users.noreply.github.com>
2026-08-10 08:05:15 -0400

    parity

--- start.js
+++ start.js
@@ -10,11 +10,11 @@ import { worker, wrmHost, getHostRoomId } from "./host.js";
     console.log("# STARTING WORKER")
     await worker.start(
         "warfront.js", // gamemode (see configs folder for other gamemodes)
-        "[OFFICAL] Warfront", // room name
-        "A stable server hosted by the developer of the game.",
+        "[WOMS] Warfront", // room name
+        "The default gamemode for the Woomy Online Modded Server\nhttps://github.com/DrakoHyena/woomy-online-modded-server\n(To change this, edit start.js)", // room description
         99, // max players (note, player limit will intentionally be hidden if it's 99)
         20, // default bot amount
-        "suckmyballs", // default host powers token
+        "roomhost", // default host powers token
     )
 
     console.log("🚀 Up and running with room id: " + await getHostRoomId())
